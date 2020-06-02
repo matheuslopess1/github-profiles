8d39bf6198734407fe289c0345379f69918cd7b3
Matheus I Lopes <matheuslopes1@protonmail.com>
2020-06-01 23:40:00 -0300

    buscando da api

--- src/main.js
+++ src/main.js
@@ -1,23 +1,69 @@
-function App() {
-  const repositories = [];
+import api from "./api";
 
-  const formEl = document.getElementById("repo-form");
+function App() {
+  const repositories = [],
+    formEl = document.getElementById("repo-form"),
+    inputEl = document.querySelector("input[name=repository]"),
+    listEl = document.getElementById("repo-list");
 
   function registerHandlers() {
     formEl.onsubmit = (event) => addRepository(event);
   }
 
-  function addRepository(event) {
+  async function addRepository(event) {
     event.preventDefault();
 
+    const repoInput = inputEl.value;
+
+    if (repoInput.length === 0) return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    console.log(response);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url },
+    } = response.data;
+
     repositories.push({
-      name: "rocketseat.com.br",
-      description: "Tire a sua ideia do papel e dê vida à sua startup.",
-      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
-      html_url: "http://github.com/rocketseat/rocketseat.com.br",
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
 
-    console.log(repositories);
+    this.inputEl.value = "";
+
+    render();
+  }
+
+  function render() {
+    listEl.innerHTML = "";
+
+    repositories.forEach((repo) => {
+      let imgEl = document.createElement("img"),
+        titleEl = document.createElement("strong"),
+        descriptionEl = document.createElement("p"),
+        linkEl = document.createElement("a"),
+        listItemEl = document.createElement("li");
+
+      imgEl.setAttribute("src", repo.avatar_url);
+      titleEl.appendChild(document.createTextNode(repo.name));
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      listEl.appendChild(listItemEl);
+    });
   }
 
   registerHandlers();
